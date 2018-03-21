79ecb05c64cd2934c34cf5b79ed80b6221d01175
Slobodan <slobodan.kosanovic@gmail.com>
2018-03-21 18:26:12 +0100

    a

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,13 @@ public class Hello {
     public static void main(String[] args){
 
         System.out.println("Hello, Kole!");
+
         int myFirstNumber = 5;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println(myTotal);
     }
 }
